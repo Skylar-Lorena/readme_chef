4a453ca32c2da6432fe9095170cdd4ca29cee03b
Skylar-Lorena <lorayne911@gmail.com>
2025-07-21 10:50:16 +0300

    feat: create Home page layout and import all components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ReadMeEditor from "../components/ReadMeEditor";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <ReadMeEditor />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
